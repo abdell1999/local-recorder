ad135331c069d6de12b8ec09f0d62746a5f0a723
Abdellah Bukad-duh Rappani <abdellahbukadduh99@gmail.com>
2026-06-22 17:28:02 +0200

    Añade esqueleto PWA con caché de modelos de Hugging Face

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
 export default defineNuxtConfig({
   compatibilityDate: '2026-06-22',
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vite-pwa/nuxt'],
   devtools: { enabled: true },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Local Recorder',
+      short_name: 'LocalRecorder',
+      description: 'Grabación, transcripción y exportación 100% local en el navegador',
+      theme_color: '#1d4ed8',
+      background_color: '#ffffff',
+      display: 'standalone',
+      icons: [{ src: 'icon.svg', sizes: 'any', type: 'image/svg+xml' }],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,ico}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/huggingface\.co\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'whisper-models',
+            cacheableResponse: { statuses: [0, 200] },
+            expiration: { maxEntries: 20, maxAgeSeconds: 31536000 },
+          },
+        },
+      ],
+    },
+  },
 })
