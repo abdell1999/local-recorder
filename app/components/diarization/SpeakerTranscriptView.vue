69b7f02bd8b800ff7c73a336a4f364716c20de53
Abdellah Bukad-duh Rappani <abdellahbukadduh99@gmail.com>
2026-06-25 18:54:21 +0200

    Añade SpeakerTranscriptView para mostrar transcripción por hablante

---
+++ app/components/diarization/SpeakerTranscriptView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { AlignedSpeakerBlock } from '../../utils/speakerTranscriptAlignment'
+
+defineProps<{
+  blocks: AlignedSpeakerBlock[]
+}>()
+</script>
+
+<template>
+  <div class="space-y-4">
+    <div v-for="(block, index) in blocks" :key="index" class="space-y-1">
+      <p
+        v-if="block.globalSpeakerId !== null"
+        class="text-sm font-semibold text-gray-700"
+      >
+        Hablante {{ block.globalSpeakerId + 1 }}
+      </p>
+      <p class="text-gray-900 whitespace-pre-wrap">{{ block.text }}</p>
+    </div>
+  </div>
+</template>
